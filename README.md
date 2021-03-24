2d8979c7a1d8288ac2172f41b9282df3c3fb8cbc
Till Konnegen <till.konnegen@outlook.de>
2021-03-24 18:13:59 +0100

    🚀 Uploaded Files

---
+++ README.md
@@ -0,0 +1,1 @@
+Overwrite the files in `/var/www/html` inside the docker with the ones in this repo to get the dark theme for polr.
---
+++ resources/views/about.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.base')
+
+@section('css')
+<link rel='stylesheet' href='/css/about.css' />
+<link rel='stylesheet' href='/css/effects.css' />
+@endsection
+
+@section('content')
+<div class='well logo-well'>
+    <img class='logo-img' src='/img/logo.png' />
+</div>
+
+<div class='about-contents'>
+    @if ($role == "admin")
+    <dl>
+        <p>Build Information</p>
+        <dt>Version: {{env('POLR_VERSION')}}</dt>
+        <dt>Release date: {{env('POLR_RELDATE')}}</dt>
+        <dt>App Install: {{env('APP_NAME')}} on {{env('APP_ADDRESS')}} on {{env('POLR_GENERATED_AT')}}<dt>
+    </dl>
+    <p>You are seeing the information above because you are logged in as an administrator.</p>
+    @endif
+
+    <p>{{env('APP_NAME')}} is powered by Polr 2, an open source, minimalist link shortening platform.</p>
+</div>
+
+@endsection
+
+@section('js')
+@endsection
